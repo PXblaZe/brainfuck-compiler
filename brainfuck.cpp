cf063f88f36a94f2a480ce4b6bf66c8159935466
PXblaZe <gsumit178@gmail.com>
2024-01-03 14:58:53 +0530

    fet: added new optimizations.

--- brainfuck.cpp
+++ brainfuck.cpp
@@ -1,4 +1,5 @@
 #include <stack>
+#include <string>
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -14,17 +15,43 @@ Brainfuck::Brainfuck(const char *filename)
         exit(-1);
     }
 
+    char lc = -1;
+    size_t ct = 0;
 
     for (char c = fgetc(pfile); c != EOF; c = fgetc(pfile)) 
     {
-        if (c == '.' || c == ',' || c == '<' || c == '>' || c == '[' || c == ']' || c == '+' || c == '-')
-            instr_mem.push_back(c);
+        if (!(c == '.' || c == ',' || c == '<' || c == '>' || c == '[' || c == ']' || c == '+' || c == '-')) continue;
+        
+        if (c == '<' || c == '>' || c == '+' || c == '-')
+        {
+            if (!ct) lc = c;
+            if (lc == c) ct++;
+            else {
+                if (ct > 1) {
+                    std::string ns = std::to_string(ct);
+                    instr_mem.insert(instr_mem.end(), ns.cbegin(), ns.cend());
+                }
+                instr_mem.push_back(lc);
+                ct = 1, lc = c;
+            }
+        }
+        else {
+            if (!ct) instr_mem.push_back(c);
+            else if (lc != c)
+            {
+                if (ct > 1) {
+                    std::string ns = std::to_string(ct);
+                    instr_mem.insert(instr_mem.cend(), ns.cbegin(), ns.cend());
+                }
+                instr_mem.insert(instr_mem.cend(), {(unsigned char)lc, (unsigned char)c});
+                ct = 0, lc = -1;
+            }
+        }
+
     }
 
-    fclose(pfile);
- 
+    fclose(pfile); 
     if (instr_mem.size() > 0) pByte = static_cast<unsigned char*>(malloc(sizeof(unsigned char)));
-    
 }
 
 Brainfuck::~Brainfuck()
@@ -32,7 +59,7 @@ Brainfuck::~Brainfuck()
     if (pByte != nullptr) free(pByte);
 }
 
-int Brainfuck::execute()
+int Brainfuck::execute(BF_on_dot_func on_dot, BF_on_comma_func on_comma)
 {
     std::stack<std::vector<unsigned char>::const_iterator> loops;
     for (std::vector<unsigned char>::const_iterator pIM = instr_mem.cbegin(); pIM != instr_mem.cend(); pIM++)
@@ -40,27 +67,62 @@ int Brainfuck::execute()
         switch (*pIM)
         {
             case '>':
+                if (pIM > instr_mem.cbegin() && isdigit(*(pIM-1))) {
+                    std::vector<unsigned char>::const_iterator fdc = pIM -1, ldc = fdc;
+                    while (fdc > instr_mem.cbegin() && isdigit(*(--fdc)));
+                    if (!isdigit(*fdc)) fdc++;
+                    size_t n = stosz(fdc.base(), ldc.base()); // static_cast<size_t>(std::strtoll((char*)fdc.base(), nullptr, 10));
+                    data_mem += n;
+                    break;
+                }
                 data_mem++;
                 break;
 
             case '<':
+                if (pIM > instr_mem.cbegin() && isdigit(*(pIM-1))) {
+                    std::vector<unsigned char>::const_iterator fdc = pIM -1, ldc = fdc;
+                    while (fdc > instr_mem.cbegin() && isdigit(*(--fdc)));
+                    if (!isdigit(*fdc)) fdc++;
+                    size_t n = stosz(fdc.base(), ldc.base());// static_cast<size_t>(std::strtoll((char*)fdc.base(), nullptr, 10));
+                    data_mem -= n;
+                    break;
+                }
                 data_mem--;
                 break;
 
             case '+':
+                if (pIM > instr_mem.cbegin() && isdigit(*(pIM-1))) {
+                    std::vector<unsigned char>::const_iterator fdc = pIM -1, ldc = fdc;
+                    while (fdc > instr_mem.cbegin() && isdigit(*(--fdc)));
+                    if (!isdigit(*fdc)) fdc++;
+                    size_t n = stosz(fdc.base(), ldc.base());// static_cast<size_t>(std::strtoll((char*)fdc.base(), nullptr, 10));
+                    (*data_mem) += n;
+                    break;
+                }
                 (*data_mem)++;
                 break;
             
             case '-':
+                if (pIM > instr_mem.cbegin() && isdigit(*(pIM-1))) {
+                    std::vector<unsigned char>::const_iterator fdc = pIM -1, ldc = fdc;
+                    while (fdc > instr_mem.cbegin() && isdigit(*(--fdc)));
+                    if (!isdigit(*fdc)) fdc++;
+                    size_t n = stosz(fdc.base(), ldc.base());// static_cast<size_t>(std::strtoll((char*)fdc.base(), nullptr, 10));
+                    (*data_mem) -= n;
+                    break;
+                }
                 (*data_mem)--;
                 break;
 
             case '.':
-                on_dot(data_mem.getData());
+                if (on_dot == nullptr) _on_dot(data_mem.getData());
+                else on_dot(data_mem.getData());
                 break;
 
             case ',':
-                on_comma(pByte), data_mem.setData(*pByte);
+                if (on_comma == nullptr) _on_comma(pByte);
+                else on_comma(pByte);
+                data_mem.setData(*pByte);
                 break;
 
             case '[':
@@ -97,15 +159,26 @@ int Brainfuck::execute()
 
         }
     }
+    if (!loops.empty()) {
+        fprintf(stderr, "error: UNBALANCED `[` BRACKETS.\n");
+        return -1;
+    }
     return 0;
 }
 
-void Brainfuck::on_dot(unsigned char data)
+inline size_t Brainfuck::stosz(const unsigned char* first, const unsigned char* last) const
+{
+    size_t res = *first - '0';
+    while (first++ != last) res = res*10 + *first - '0';
+    return res;
+}
+
+void Brainfuck::_on_dot(unsigned char data)
 {
     printf("%c", data);
 }
 
-void Brainfuck::on_comma(unsigned char* pByte)
+void Brainfuck::_on_comma(unsigned char* pByte)
 {
     scanf("%c", pByte);
 }
--- brainfuck.hpp
+++ brainfuck.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <vector>
 #include "bfBuffer.hpp"
 
 class Brainfuck
@@ -8,12 +9,18 @@ class Brainfuck
     bf_buffer<unsigned char> data_mem;
     std::vector<unsigned char> instr_mem;
 
+    void _on_dot(unsigned char data);
+    void _on_comma(unsigned char* pByte);
+    inline size_t stosz(const unsigned char* first, const unsigned char* last) const;
+
 public:
 
     Brainfuck(const char* filename);
     ~Brainfuck();
 
-    int execute();
-    void on_dot(unsigned char data);
-    void on_comma(unsigned char* pByte);
+    typedef void (*BF_on_dot_func)(unsigned char data);
+    typedef void (*BF_on_comma_func)(unsigned char* pByte);
+
+    int execute(BF_on_dot_func on_dot = nullptr, BF_on_comma_func on_comma = nullptr);
+
 };
