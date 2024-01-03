85114434bd954564e3cecc7a0eb09050cc0bc0ca
PXblaZe <gsumit178@gmail.com>
2024-01-03 15:00:36 +0530

    impl: implemented the bf_buffer class using std::deque.

--- bfBuffer.hpp
+++ bfBuffer.hpp
@@ -1,37 +1,24 @@
 #pragma once
 
-#include <vector>
+#include <deque>
 
-template<typename _Tp, _Tp def_val = _Tp()>
+
+template<typename _Tp, const _Tp def_val = _Tp()>
 class bf_buffer
 {
-    std::vector<_Tp> forward, backward;
-    _Tp* iter = nullptr; 
-
-    void push_back(const _Tp& data)
-    {
-        forward.push_back(data);
-    }
-
-    void push_front(const _Tp& data)
-    {
-        backward.push_back(data);
-    }
-
+    std::deque<_Tp> buffer;
+    typename std::deque<_Tp>::iterator iter;
 
 public:
-
-
-
-    bf_buffer(const _Tp& data = def_val) : forward(1, data) {
-        iter = &forward[0];
+    bf_buffer(const _Tp& data = def_val) : buffer(1, data) {
+        iter = buffer.begin();
     }
 
     _Tp& operator[](int) const = delete;
 
     inline void setData(const _Tp& data)
     {
-        if (iter != nullptr) *iter = data;
+        *iter = data;
     }
 
     inline constexpr const _Tp& getData() const
@@ -41,87 +28,67 @@ public:
 
     inline constexpr size_t size(bool inbytes = false) const noexcept
     {
-        return inbytes ? sizeof(_Tp)*(forward.size() + backward.size())
-                        : forward.size() + backward.size();
+        return inbytes ? sizeof(_Tp) * buffer.size() : buffer.size();
     }
 
     inline constexpr size_t size_left2pointer(bool inbytes = false) const noexcept
     {
-        return size(inbytes) - size_right2pointer(inbytes) -1;
+        return size(inbytes) - size_right2pointer(inbytes) - 1;
     }
 
     inline constexpr size_t size_right2pointer(bool inbytes = false) const noexcept
     {
-        size_t s = 0;
-        if (iter >= forward.begin().base() && iter <= forward.begin().base())
-            s = forward.end().base() - iter -1;
-        else if (iter >= backward.begin().base() && iter <= backward.begin().base())
-            s = iter - backward.begin().base() + forward.size();
-        return inbytes ? sizeof(_Tp)*s : s;
+        size_t s = buffer.end() - iter -1;
+        return inbytes ? sizeof(_Tp) * s : s;
     }
 
-    inline constexpr _Tp&  operator*()
+    inline constexpr _Tp& operator*()
     {
         return *iter;
     }
 
-    const _Tp* operator++()
+    inline typename std::deque<_Tp>::iterator operator++()
     {
-        if (iter == backward.begin().base()) 
-        {
-            iter = forward.begin().base();
-            return iter;
-        }
-        else if (iter+1 == forward.end().base())
-        {
-            push_back(def_val), iter = forward.end().base()-2;
-        }
-        return iter >= forward.begin().base() && iter <= forward.end().base() ? ++iter : --iter;
-    } 
+        if (iter == buffer.end()-1) buffer.push_back(def_val), iter = buffer.end()-1;
+        else return ++iter;
+        return iter;
+    }
 
-        
-    const _Tp* operator++(int)
+    inline typename std::deque<_Tp>::iterator operator++(int)
     {
-        if (iter == backward.begin().base()) 
-        {
-            iter = forward.begin().base();
-            return backward.begin().base();
-        }
-        else if (iter == forward.end().base()-1)
-        {
-            push_back(def_val), iter = forward.end().base()-2;
-        }
-        return iter >= forward.begin().base() && iter <= forward.end().base() ? iter++ : iter--;
-    } 
+        if (iter == buffer.end()-1) buffer.push_back(def_val), iter = buffer.end()-1;
+        else return iter++;
+        return iter-1;
+    }
 
-    const _Tp* operator--()
+    inline typename std::deque<_Tp>::iterator operator--()
     {
-        if (iter == forward.begin().base()) 
-        {
-            if (backward.empty()) push_front(def_val);
-            iter = backward.begin().base();
-            return iter;
-        }
-        else if (iter == backward.end().base()-1)
-        {
-            push_front(def_val), iter = backward.end().base()-2;
-        }
-        return iter >= forward.begin().base() && iter <= forward.end().base() ? --iter : ++iter;
-    } 
+        if (iter == buffer.begin()) buffer.push_front(def_val), iter = buffer.begin();
+        else return --iter;
+        return iter;
+    }
 
-     const _Tp* operator--(int)
+    inline typename std::deque<_Tp>::iterator operator--(int)
     {
-        if (iter == forward.begin().base()) 
-        {
-            if (backward.empty()) push_front(def_val);
-            iter = backward.begin().base();
-            return forward.begin().base();
-        }
-        else if (iter == backward.end().base()-1)
-        {
-            push_front(def_val), iter = backward.end().base()-2;
-        }
-        return iter >= forward.begin().base() && iter <= forward.end().base() ? iter-- : iter++;
-    } 
+        if (iter == buffer.begin()) buffer.push_front(def_val), iter = buffer.begin();
+        else return iter--;
+        return iter+1;
+    }
+
+    template<typename _DT>
+    inline typename std::deque<_Tp>::iterator operator+=(const _DT& val)
+    {
+        if (iter+val >= buffer.end()) buffer.resize(iter-buffer.begin()+val+1, def_val), iter = buffer.end()-1;
+        else iter += val;
+        return iter;
+    }
+
+    template<typename _DT>
+    inline typename std::deque<_Tp>::iterator operator-=(const _DT& val)
+    {
+        if (iter-val < buffer.begin()) buffer.insert(buffer.cbegin(), buffer.begin()-iter+val, def_val), iter = buffer.begin();
+        else iter -= val;
+        return iter;
+    }
 
 };
